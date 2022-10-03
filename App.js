4d72ba78fb25cbfebc1f172a035475a3843cbe91
Jinbee <jbl624@hufs.ac.kr>
2022-10-03 16:30:22 +0900

    add

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, ScrollView, View, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+     <>
+      <View style={[styles.container, styles.playingSpace]}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </View>
+      <ScrollView style={[styles.container]}>
+        <View style={[styles.controlSpace]}>
+         <View style={[styles.buttonView]}>
+            <Button title = "Change flex Direction"/>
+          </View>
+          <View style={[styles.buttonView]}>
+            <Button title = "Change Justify Content"/>
+          </View>
+          <View style={[styles.buttonView]}>
+            <Button title = "Change Align Items"/>
+          </View>
+          <View style={[styles.buttonView]}>
+            <Button title = "Change Direction"/>
+          </View>
+          <View style={[styles.buttonView]}>
+            <Button title = "Change flex wrap"/>
+          </View>
+          <View style={[styles.buttonView]}>
+            <Button title = "add square"/>
+          </View>
+          <View style={[styles.buttonView]}>
+            <Button title = "Delete Square"/>
+          </View>
+        </View>
+      </ScrollView>
+     </>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      height:'50%'
+    },
+    playingSpace:{
+      backgroundColor: "grey",
+      borderColor: "blue",
+      borderWidth: 3
+    },
+    controlSpace: {
+      backgroundColor: "F5F5F5",
+      flexDirection: 'row',
+      flexWrap: 'wrap'
+    },
+    buttonView:{
+      backgroundColor: "skyblue",
+      width: '47%',
+      //padding: 10,
+      marginLeft: 5,
+      margin: 5
+    }
+  });
+
+  export default App;
