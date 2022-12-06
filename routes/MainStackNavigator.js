c7a2b582df5430928942802b8d070f3162b238ec
JinbeeLee <114037074+JinbeeLee@users.noreply.github.com>
2022-12-06 16:45:59 +0900

    Add files via upload

---
+++ routes/MainStackNavigator.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Layout from '../screens/Layout';
+import Home from '../screens/Home';
+
+const Nav = createStackNavigator();
+
+function MainStackNavigator() {
+    return (
+        <NavigationContainer>
+            <Nav.Navigator initialRouteName='Home'>
+                <Nav.Screen name = "Layout" component={Layout} options={{title: '처음 Layout'}}/>
+                <Nav.Screen name = "Home" component={Home} options={{title: '추가 Home'}}/>
+
+            </Nav.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MainStackNavigator;
---
+++ routes/MainTabNavigator.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Layout from '../screens/Layout';
+
+const Nav = createBottomTabNavigator();
+
+function MainTabNavigator() {
+    return (
+        <NavigationContainer>
+            <Nav.Navigator initialRouteName='Home'>
+                <Nav.Screen name = "Layout" component={Layout} options={{title: '처음 Layout'}}/>
+                <Nav.Screen name = "Home" component={Home} options={{title: '추가 Home'}}/>
+
+            </Nav.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MainTabNavigator;
