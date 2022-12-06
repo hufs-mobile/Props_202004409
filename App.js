133404b1bec158b0dd12de355b826d3f73313eab
Jinbee <jbl624@hufs.ac.kr>
2022-12-06 15:21:24 +0900

    onPress 추가

--- App.js
+++ App.js
@@ -6,58 +6,44 @@ const App = () => {
   return (
     <>
     <View style={{paddingTop: Constants.statusBarHeight}}></View>
-    <ScrollView style={[styles.container, styles.playingSpace]}>
-      <Text>Hi, I'm writing a text.</Text>
-      <Text>Hi, I'm writing a text again.</Text>
-      <Text>
-        Do you remember
-        The 21st night of September?
-        Love was changin' the minds of pretenders
-        While chasin' the clouds away
-        Our hearts were ringin'
-        In the key that our souls were singin'
-        As we danced in the night, remember
-        How the stars stole the night away, oh, yeah
-        Hey, hey, hey
-        Ba-dee-ya, say, do you remember?
-        Ba-dee-ya, dancin' in September
-        Ba-dee-ya, never was a cloudy day</Text>
-      <Image source = {require('./assets/favicon.png')}/>
-      
-      <ImageBackground
-        style = {{width:300, height:300, resizeMode: 'contain'}}
-        source = {{uri: 'https://www.rollingstone.com/wp-content/uploads/2018/06/rs-226972-GettyImages-74269926.jpg?w=1581&h=1054&crop=1'}}
-        >
-          <View>
-            <Text>September by Earth Fire & Wind</Text>
-          </View> 
-      </ImageBackground>
-      
-      {/*<Image source = {{uri: 'http://reactnative.dev/img/tiny_logo.png'}}/>*/}
-      
-    </ScrollView>
+    <ScrollView style={[styles.container, styles.playingSpace]}/>
+    
     <ScrollView style={[styles.container]}>
       <View style={[styles.controlSpace]}>
         <View style={[styles.buttonView]}>
-          <Button title = "Change flex Direction"/>
+          <Button title = "Change flex Direction"
+            onPress={()=>console.log("CHNAGE FLEX DIRECTION")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Change Justify Content"/>
+          <Button title = "Change Justify Content"
+            onPress={()=>console.log("CHNAGE JUSTIFY CONTENT")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Change Align Items"/>
+          <Button title = "Change Align Items"
+            onPress={()=>console.log("CHNAGE ALIGN ITEMS")}
+          />
         </View>
           <View style={[styles.buttonView]}>
-          <Button title = "Change Direction"/>
+          <Button title = "Change Direction"
+            onPress={()=>console.log("CHNAGE DIRECTION")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Change flex wrap"/>
+          <Button title = "Change flex wrap"
+            onPress={()=>console.log("CHNAGE FLEX WRAP")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Add square"/>
+          <Button title = "Add square"
+            onPress={()=>console.log("ADD SQUARE")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Delete Square"/>
+          <Button title = "Delete Square"
+            onPress={()=>console.log("DELETE SQUARE")}
+          />
         </View>
       </View>
     </ScrollView>
