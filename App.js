906e7d81a12464c3c0deec5fcecae5f5ff7fc25d
Jinbee <jbl624@hufs.ac.kr>
2022-12-06 16:04:28 +0900

    flexDirection and etc.

--- App.js
+++ App.js
@@ -5,9 +5,43 @@ import { useState } from "react";
 
 const App = () => {
 
-  const flexDirections = ['row', 'rew-reverse', 'column', 'column-reverse'];
+  const flexDirections = ['row', 'row-reverse', 'column', 'column-reverse'];
+  const justifyContents = [
+    'flex-start',
+    'flex-end',
+    'center',
+    'space-between',
+    'space-around',
+    'space-evenly',
+  ];
+  const alignItemsArr = ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'];
+  const wraps = ['nowrap', 'wrap', 'wrap-reverse'];
+  const directions = ['inherit', 'ltr', 'rtl'];
 
   const [flexDirection, setFlexDirection] = useState(0);
+  const [justifyContent, setJustifyContent] = useState(0);
+  const [alignItems, setAlignItems] = useState(0);
+  const [direction, setDirection] = useState(0);
+  const [wrap, setWrap] = useState(0);
+
+  const hookedStyles = {
+    flexDirection: flexDirections[flexDirection],
+    justifyContent: justifyContents[justifyContent],
+    alignItems: alignItemsArr[alignItems],
+    direction: directions[direction],
+    flexWrap: wraps[wrap],
+  };  
+
+  const changeSetting = (value, options, setterfunction) => {
+    var targetValue = 0;
+    if (value == options.length - 1) {
+      targetValue = 0;    
+    } else {
+      targetValue = value + 1;
+    }
+    setterfunction(targetValue);
+    console.log(options[targetValue]);
+  };
 
   const Square = () => {
     const sqStyle = {
@@ -23,39 +57,39 @@ const App = () => {
   return (
     <>
     <View style={{paddingTop: Constants.statusBarHeight}}></View>
-    <ScrollView style={[styles.container, styles.playingSpace]}>
+    <View style={[styles.container, styles.playingSpace, hookedStyles]}>
       { squares.map(elem => elem) }    
-      </ScrollView>
+    </View>
 
     <ScrollView style={[styles.container]}>
       <View style={[styles.controlSpace]}>
         <View style={[styles.buttonView]}>
           <Button title = "Change flex Direction"
-            onPress={()=>console.log("CHNAGE FLEX DIRECTION")}
+            onPress={()=> changeSetting(flexDirection, flexDirections, setFlexDirection)}
           />
         </View>
         <View style={[styles.buttonView]}>
           <Button title = "Change Justify Content"
-            onPress={()=>console.log("CHNAGE JUSTIFY CONTENT")}
+            onPress={()=>changeSetting(justifyContent, justifyContents, setJustifyContent)}
           />
         </View>
         <View style={[styles.buttonView]}>
           <Button title = "Change Align Items"
-            onPress={()=>console.log("CHNAGE ALIGN ITEMS")}
+            onPress={()=>changeSetting(alignItems, alignItemsArr, setAlignItems)}
           />
         </View>
           <View style={[styles.buttonView]}>
           <Button title = "Change Direction"
-            onPress={()=>console.log("CHNAGE DIRECTION")}
+            onPress={()=>changeSetting(direction, directions, setDirection)}
           />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Change flex wrap"
-            onPress={()=>console.log("CHNAGE FLEX WRAP")}
+          <Button title = "Change Flex Wrap"
+            onPress={()=>changeSetting(wrap, wraps, setWrap)}
           />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title = "Add square"
+          <Button title = "Add Square"
             onPress={()=>setSquares([...squares, Square()])}
           />
         </View>
